f403d6e26dda75806075d27759fa912cf9934140
Santiago Crippa <santicrippa@gmail.com>
2024-08-31 19:14:38 -0300

    HTTP Request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TaskController {
   constructor(private tasksService: TasksService) {}
-  @Get('/tasks')
+
+  @Get()
   getAllTasks() {
     return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.tasksService.createTasks();
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTasks();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTasks();
+  }
+
+  @Patch() // El metodo patch solo actualiza una porcion espeficica del objeto, a diferencia de Put, que actualiza el objeto entero
+  updateTaskStatus() {
+    return this.tasksService.updateTasksStatus();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
   getTasks() {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  createTasks() {
+    return 'Creando tareas';
+  }
+
+  updateTasks() {
+    return 'Actualizando tareas';
+  }
+
+  deleteTasks() {
+    return 'Borrando tareas';
+  }
+
+  updateTasksStatus() {
+    return 'Actualizando estatus de las tareas';
+  }
 }
